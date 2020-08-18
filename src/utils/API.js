9303738a63d3499fd985c466285e6898702b3f7a
Cameron Graham <cameron.g882@gmail.com>
2020-08-18 10:44:52 -0400

    remade react app and deployed to gh a different way

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
-export default {
-  searchUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
-  }
-};
